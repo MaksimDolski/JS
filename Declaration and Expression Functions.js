2f6b6cd9cdca1967d439f697bd37ba4684a08cd2
Max <maxdolski@gmail.com>
2020-01-31 14:15:28 +0200

    ll

---
+++ Declaration and Expression Functions.js
@@ -0,0 +1,11 @@
+// function declaration and function expression
+
+// Function declaration
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+// Function expression
+var add = function (num1, num2) {
+	return num1 + num2;
+};
