443c40fafe51d9159740fc48a58db67224dbb7e7
Max <maxdolski@gmail.com>
2020-01-31 09:20:32 +0200

    Create Callback.js

---
+++ Callback.js
@@ -0,0 +1,31 @@
+function first() {
+  // Simulate a code delay
+  setTimeout(function(){console.log(1);}, 500);
+}
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
+
+// Callback
+function doHomework(subject, callback) {
+  console.log(`Starting my ${subject} homework.`);
+  callback();
+}
+
+doHomework("math", function() {console.log("Finished my homework");});
+
+// Callback
+function doHomework(subject, callback) {
+  console.log(`Startind my ${subject} homework`);
+  callback();
+}
+
+function finished() {
+  console.log('Finished my homework!');
+}
+
+doHomework("English", finished);
