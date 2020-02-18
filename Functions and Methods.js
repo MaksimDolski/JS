52533c10dad7a6c4890ec0fe562fe970ea45ec19
Maksim <Maksim@MacBook-Pro-Maksim.local>
2020-02-18 13:01:21 +0200

    new

---
+++ Functions and Methods.js
@@ -0,0 +1,6 @@
+var a = Math.random(); // генирирует случайное число
+var a = Math.round(a); // округляет число
+var userNum = document.querySelector("#user-num");
+document.querySelector("#btn").onclick = checkNum;
+num = parseInt(num); // возвращает целое число, если будет 321e - функция вернёт 321
+location.reload(); // перезагружает код в месте вставки
