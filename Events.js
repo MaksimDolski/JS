ad1d683acb12b97daccb64281eef990c7c76cf31
Maksim <Maksim@MBP-Maksim.lan>
2020-02-20 21:15:28 +0200

    new

---
+++ Events.js
@@ -0,0 +1,21 @@
+// Events
+
+document.querySelector(".new").onclick = function () {
+  console.log("you have click");
+}
+
+document.querySelector(".double").ondblclick = function () {
+  console.log("you have double click");
+}
+
+document.querySelector(".mouse").onmousemove = function () {
+  console.log("mouse has moved");
+}
+
+document.querySelector(".mouseEnter").onmouseenter = function () {
+  console.log("mouse has moved2");
+}
+
+document.querySelector(".mouseLeave").onmouseleave = function () {
+  console.log("mouse leave");
+}
