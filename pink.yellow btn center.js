e9e39cd9639a201ba258ccdb2f4501a4ed2525b4
Maksim <Maksim@MacBook-Pro-Maksim.local>
2020-02-21 09:30:44 +0200

    new

---
+++ pink.yellow btn center.js
@@ -0,0 +1,16 @@
+document.querySelector(".new").onclick = function () {
+console.log("you have click");
+document.querySelector(".new").style.background = "yellow";
+
+}
+
+div, .new {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  border: 1px solid white;
+  background-color: pink;
+  border-radius: 10px;
+}
