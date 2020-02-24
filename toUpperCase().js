af58c42f849abfeddba5026ebbdc385950c8ba7b
Maksim <Maksim@MBP-Maksim.lan>
2020-02-24 12:03:40 +0200

    new

---
+++ toUpperCase().js
@@ -0,0 +1,11 @@
+var btn = document.querySelector('#btn');
+var line = document.querySelector('#line');
+var lineValue = line.value;
+var arr = lineValue.split();
+
+function run () {
+  for (let i = 0; i < arr.length; i++) {
+    var colors[0] = arr[0].toUpperCase();
+   }
+   console.log(colors);
+ }
