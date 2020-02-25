2c48f77a322e9d57def0e137ce46a2ce1e311c67
Maksim <Maksim@MacBook-Pro-Maksim.local>
2020-02-25 14:38:22 +0200

    chg

--- toUpperCase().js
+++ toUpperCase().js
@@ -1,11 +1,36 @@
-var btn = document.querySelector('#btn');
+var p1 = document.querySelector('#p1');
+var p2 = document.querySelector('#p2');
 var line = document.querySelector('#line');
-var lineValue = line.value;
-var arr = lineValue.split();
+var btn = document.querySelector('#btn');
 
-function run () {
-  for (let i = 0; i < arr.length; i++) {
-    var colors[0] = arr[0].toUpperCase();
-   }
-   console.log(colors);
- }
+
+function run() {
+var result = document.querySelector('#line').value;
+console.log(result);
+
+var result2 = result.toUpperCase();
+document.querySelector('#p2').innerHTML += result2;
+}
+
+// or
+
+function run() {
+var result = document.querySelector('#line').value;
+document.querySelector('#p1').innerHTML += result;
+
+var str;
+for (var i = 0; i < result.length; i++) {
+var str = result[i].toUpperCase();
+
+document.querySelector('#p2').innerHTML += str;
+}
+}
+
+// or
+document.querySelector('#p1').style.textTransform = "uppercase";
+
+<input id="line" type="text">
+<button id="btn" type="button" onclick="run(); run2()">Upper</button>
+
+<p id="p1">Lower case: </p>
+<p id="p2">Upper case: </p>
