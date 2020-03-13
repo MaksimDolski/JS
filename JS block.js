d251970c1bf8c827512e4ba2070dc5124a9afdd0
Maksim Dolski <maxdolski@gmail.com>
2020-03-13 08:48:34 +0200

    new

---
+++ JS block.js
@@ -0,0 +1,42 @@
+<link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
+<title>My Template</title>
+</head>
+
+<body>
+
+<div class="logo">
+<div class="js">JS</div>
+<div class="text">I will be happy to work in your company as a Junior Web Developer</div>
+</div>    
+
+
+.logo {
+  width: 450 px;
+  height: 200 px;
+  background - color: #F0DA50;
+  display: flex;
+  align - items: center;
+  justify - content: center;
+
+}
+
+.js {
+  font - weight: bold;
+  font - size: 95 px;
+  padding - left: 20 px;
+  letter - spacing: -8 px;
+
+}
+
+.text {
+  font - size: 25 px;
+  font - weight: 600;
+  padding - left: 20 px;
+  padding - right: 30 px;
+}
+
+.js, .text {
+  color: #323136;
+    font-family: 'Inter', sans-serif;
+
+  }
