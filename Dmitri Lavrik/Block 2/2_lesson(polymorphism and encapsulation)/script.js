9490564f0936017c11024717f5bb149756b402b1
Maksim Dolski <maxdolski@gmail.com>
2020-09-07 15:57:41 +0300

    new

--- Dmitri Lavrik/Block 2/2_lesson(polymorphism and encapsulation)/script.js
+++ Dmitri Lavrik/Block 2/2_lesson(polymorphism and encapsulation)/script.js
@@ -1,37 +1,37 @@
 // В объектно-ориентированном программировании класс – это расширяемый шаблон кода для создания объектов, который устанавливает в них начальные значения (свойства) и реализацию поведения (методы).
 
 class Animal {
-  constructor(name, weight) {
-    this.name = name;
-    this.weight = 1;
-  }
+    constructor(name, weight) {
+        this.name = name;
+        this.weight = 1;
+    }
 
-  eat() {
-    return this.weight = 177;
-  }
+    eat() {
+        return this.weight = 177;
+    }
 
-  log() {
-    return this.name + ' ' + this.weight;
-  }
+    log() {
+        return this.name + ' ' + this.weight;
+    }
 } // class Animal
 
 class Dog extends Animal {
-  constructor(name, weight) {
-    super(name, weight);
-    this.weight = 1123;
-  }
+    constructor(name, weight) {
+        super(name, weight);
+        this.weight = 1123;
+    }
 }
 
 class Cat extends Animal {
-  constructor(name, weight) {
-    super(name, weight);
-    this.lifes = 9;
-    this.weight = 12; // полиморфизм, переопределяем общее значение
-  }
+    constructor(name, weight) {
+        super(name, weight);
+        this.lifes = 9;
+        this.weight = 12; // полиморфизм, переопределяем общее значение
+    }
 
-  log() {
-    return super.log() + ' ' + this.lifes;
-  }
+    log() {
+        return super.log() + ' ' + this.lifes;
+    }
 }
 
 let animals = [];
@@ -41,21 +41,22 @@ animals.push(new Cat('Murzik1'));
 animals.push(new Cat('Murzik2'));
 
 for (let animal of animals) {
-  console.log(animal.log());
-  console.log(animal.eat());
+    console.log(animal.log());
+    console.log(animal.eat());
 }
 
 // можно заменить обычной функцией-конструктор:
 function User(name) {
-  this.name = name;
+    this.name = name;
 }
 
-User.prototype.sayHi = function () {
-  console.log(this.name);
+User.prototype.sayHi = function() {
+    console.log(this.name);
 }
 
 let user = new User('Maxim');
 user.sayHi();
 
-
 // можно ещё дополнительно делать инкапсуляцию: export and import classes from other classes
+
+// add some changes
