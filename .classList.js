50e2c73b9d7010f0c86bbb19da57ff2429efb45a
Maksim Dolski <maxdolski@gmail.com>
2020-09-11 15:54:20 +0300

    new

--- .classList.js
+++
@@ -1,8 +0,0 @@
-var block = document.querySelector('.block');
-block.style.width = "400px";
-console.log(block.style);
-
-block.classList.add("red");
-
-block.classList.remove("red");
-block.classList.contains("red") // has? true or false
